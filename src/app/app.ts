dd6c4e35d8c71797fff45d847824369c6d56a09c
Yashanshu <yashansh3b@gmail.com>
2024-11-26 12:28:27 +0000

    initial setup

---
+++ src/app/app.ts
@@ -0,0 +1,76 @@
+import {bomberFrames, snake} from '../assets/loader';
+import * as PIXI from 'pixi.js';
+
+interface BomberFrames {
+    front: string[];
+    back: string[];
+    right: string[];
+    left:  string[];
+}
+
+// Prepare frames
+const playerFrames: BomberFrames = bomberFrames;
+
+// IMPORTANT: Change this value in order to see the Hot Module Reloading!
+const currentFrame: keyof BomberFrames = 'front';
+
+
+export class GameApp {
+
+    private app: PIXI.Application;
+
+    constructor(parent: HTMLElement, width: number, height: number) {
+
+        this.app = new PIXI.Application({width, height, backgroundColor : 0x000000});
+        parent.replaceChild(this.app.view, parent.lastElementChild); // Hack for parcel HMR
+
+        // init Pixi loader
+        let loader = new PIXI.Loader();
+
+        // Add user player assets
+        console.log('Player to load', playerFrames);
+        Object.keys(playerFrames).forEach(key => {
+            loader.add(playerFrames[key]);
+        });
+
+        // add snake
+        PIXI.Loader.shared
+        .add([snake])
+        .load(this.setup.bind(this))
+
+        // Load assets
+        loader.load(this.onAssetsLoaded.bind(this));
+    }
+
+    private onAssetsLoaded() {
+
+        const playerIdle: PIXI.AnimatedSprite = new PIXI.AnimatedSprite(playerFrames[currentFrame].map(path => PIXI.Texture.from(path)));
+
+        /*
+        * An AnimatedSprite inherits all the properties of a PIXI sprite
+        * so you can change its position, its anchor, mask it, etc
+        */
+        playerIdle.x = 100;
+        playerIdle.y = 150;
+        playerIdle['vx'] = 1;
+        playerIdle.anchor.set(0, 1);
+        // playerIdle.anchor.set(0.5);
+        playerIdle.animationSpeed = 0.3;
+        playerIdle.play();
+
+        this.app.stage.addChild(playerIdle);
+    }
+    
+    private setup() {
+  //Create the cat sprite
+    const snakeSprite = new PIXI.Sprite(PIXI.Loader.shared.resources[snake].texture);
+        console.log('snake to load', snakeSprite);
+    snakeSprite.x = 200;
+    snakeSprite.y = 300;
+    snakeSprite['vx'] = 1;
+    snakeSprite.anchor.set(0, 1);
+    this.app.stage.addChild(snakeSprite);
+} 
+
+}
+
